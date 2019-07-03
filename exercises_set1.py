3b2fef9440601dea83cbe784abd5deae58f54864
John Ogungbesan <lanogun@yahoo.co.uk>
2019-07-03 16:09:26 +0100

    Second commit

--- exercises_set1.py
+++ exercises_set1.py
@@ -62,4 +62,61 @@ b = a - 10
 print(a > b)
 
 # Exercise 14: What Boolean will be the output of the following?
-# What Boolean will be the output of the following: 12 != 12
+# 12 != 12 Ans: False
+
+# Exercise 15: What Boolean will be the output of the following?
+# 2 < 3 <10  Ans: True
+
+# Exercise 16: User for, split() and if to create a Statement that will print out words that start with 's':
+st = 'Print only the words that start with s in this sentence'
+for word in st.split():
+    if word[0] == 's':
+        print(word)
+
+# Exercise 17: User range() to print all the even numbers from 0 to 10.
+for num in range(0,11,2):
+    print(num)
+
+# Exercise 18: Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
+mylist = [num for num in range(0,51,3) if num % 3 == 0 ]
+print(mylist)
+
+# Exercise 19: Go through the string below and if the length of a word is even print "even!"
+st = 'Print every word in this sentence that has an even number of letters'
+for word in st.split():
+    if len(word) % 2 == 0:
+        print(word)
+
+# Exercise 20: Write a program that prints the integers from 1 to 100. But for multiples of three, print "Fizz"
+# instead of the number, and for multiples of five print "Buzz". For numbers which are multiples of three and
+# five, print "FizzBuzz".
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
+
+# Exercise 21: Use List Comprehension to create a list of the first letters of every word in the string below;
+st = 'Create a list of the first letters of every word in this string'
+mylist = [word[0] for word in st.split()]
+print(mylist)
+
+# Exercise 22: Write a program to count how many upper case and lower case letters in this string
+st = "Hello Mr. Rogers, how are you this fine Tuesday"
+# Output should be:
+# Uppercase: 4
+# Lowercase: 33
+
+Uppercase = 0
+Lowercase = 0
+for num in range(len(st)):
+    if st[num].isupper():
+        Uppercase += 1
+    elif st[num].islower():
+        Lowercase += 1
+print(Uppercase)
+print(Lowercase)
